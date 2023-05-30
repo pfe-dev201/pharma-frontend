9952ddb144e1c415054db37e19337460b9dbd9b2
mouadallaouiLUC <mouad.allaoui@luceom.com>
2023-05-30 18:12:20 +0100

    [PHARMA-28] Login view

--- src/views/login/loginView.jsx
+++ src/views/login/loginView.jsx
@@ -1,8 +1,21 @@
 import React from "react";
+import LoginForm from "../../components/loginPage/form";
+import background from "../../assets/images/landing.jpg";
+import logo from "../../assets/images/pharma.png";
+import "./loginView.css";
 
-function LoginView () {
+function LoginView() {
   return (
-    <></>
+    <div className="LoginPage">
+      <img src={background} alt="" className="LoginBackgound"/>
+      <div className="overBackround">
+        <LoginForm/>
+      </div>
+      <div className="Logo">
+        <img src={logo} alt="" className="LoginLogo"/>
+        <h3 className="LoginLogoName">Pharma</h3>
+      </div>
+    </div>
   );
 }
 
