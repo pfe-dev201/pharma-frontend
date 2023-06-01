1a420e7d6ed683e144e135a2da6c449283bdf89e
MouadALLAOUI <moadallaoui1@gmail.com>
2023-06-01 18:10:53 +0100

    [PHARMA-36] Back-end Login

--- src/views/login/loginView.jsx
+++ src/views/login/loginView.jsx
@@ -1,18 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import LoginForm from "../../components/loginPage/form";
 import background from "../../assets/images/landing.jpg";
 import logo from "../../assets/images/pharma.png";
 import "./loginView.css";
+import getEnvironnement from "../../environnement";
+import axios from "axios";
 
 function LoginView() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  // const [errorLogin, setErrorLogin] = useState(null);
+
+  const navigate = useNavigate();
+  const url = `${getEnvironnement().API_URL}/login`;
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    axios
+      .post(url, { email, password })
+      .then((response) => {
+        console.log(response.data);
+        // Redirect to "/pharma"
+        navigate("/pharma");
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+
   return (
     <div className="LoginPage">
-      <img src={background} alt="" className="LoginBackgound"/>
+      <img src={background} alt="" className="LoginBackgound" />
       <div className="overBackround">
-        <LoginForm/>
+        <LoginForm
+          setEmail={setEmail}
+          setPassword={setPassword}
+          handleSubmit={handleSubmit}
+        />
       </div>
       <div className="Logo">
-        <img src={logo} alt="" className="LoginLogo"/>
+        <img src={logo} alt="" className="LoginLogo" />
         <h3 className="LoginLogoName">Pharma</h3>
       </div>
     </div>
