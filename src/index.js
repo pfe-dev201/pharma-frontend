6d4964c6c9fb2d4f7d6a46f2b3777ed7d95b81a7
MouadALLAOUI <moadallaoui1@gmail.com>
2023-06-03 18:27:44 +0100

    [PHARMA-37] View Rapport

--- src/index.js
+++ src/index.js
@@ -6,14 +6,11 @@ import PharmaView from "./views/pharma/pharmaView";
 import EntreesView from "./views/pharma/content/entrees/entreesView";
 import Welcome from "./views/pharma/content/welcome/welcome";
 import SortiesView from "./views/pharma/content/sorties/sortiesView";
-<<<<<<< Updated upstream
 import ReglagesView from "./views/pharma/content/reglages/reglagesView";
 import { Provider } from "react-redux";
 import store from "./store/store";
 import CheckLogin from "./components/checkLogin";
-=======
 import Rapport from "./views/pharma/content/rapport/rapport";
->>>>>>> Stashed changes
 
 const router = createBrowserRouter([
   {
@@ -24,17 +21,13 @@ const router = createBrowserRouter([
     path: "pharma",
     element: <CheckLogin Component={PharmaView} />,
     children: [
-      { 
+      {
         index: true,
-        element: <Welcome /> 
+        element: <Welcome />
       },
       {
         path: "rapport",
-<<<<<<< Updated upstream
-        element: <CheckLogin Component={LoginView} />,
-=======
-        element: <Rapport />,
->>>>>>> Stashed changes
+        element: <CheckLogin Component={Rapport} />,
       },
       {
         path: "entrees",
